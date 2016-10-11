5ac1ec46f8bf1764079224df6e99065beda7f4bb
kovacmarko168 <kovacmarko168@gmail.com>
2016-10-11 12:46:53 +0200

    version 1.0

---
+++ version1.0/app/src/main/java/com/example/kovac94/meteorshower/GameSetup.java
@@ -0,0 +1,31 @@
+package com.example.kovac94.meteorshower;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class GameSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //fullscreen mode
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_game_setup);
+
+    }
+}
